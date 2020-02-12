aa9ee7d75b1f2e4ea6b73e540c17081ff7641e99
alexg9010 <alex.gos90@gmail.com>
2020-02-12 13:39:56 +0100

    fix missspelling of np.asarray function

--- molpy/util.py
+++ molpy/util.py
@@ -2,6 +2,6 @@ import numpy as np
 
 def distance(point1, point2):
 	
-	point1 = np.asarry(point1)
-	point2 = np.asarry(point2)
+	point1 = np.asarray(point1)
+	point2 = np.asarray(point2)
 	return np.linalg.norm(point1 - point2)
