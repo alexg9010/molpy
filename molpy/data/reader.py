8b87d8325aac4e52f0c7bdfed2200b24b3abffa6
alexg9010 <alex.gos90@gmail.com>
2020-02-13 09:37:00 +0100

    add get_molecule function to read xyz files

--- molpy/data/reader.py
+++ molpy/data/reader.py
@@ -1,7 +1,15 @@
 import os
 
+# from ..util import read_xyz     ## python3 way 
+# from molpy import util          ## import whole util, not recommmende
+from molpy.util import read_xyz ## python2 way, independent of relative file location
+
 dirname = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(dirname, 'look_and_say.dat')
 
 with open(filename, 'r') as handle:
     look_and_say = handle.read()
+
+
+def get_molecule(filename):
+    return read_xyz(os.path.join(dirname, filename + ".xyz"))
