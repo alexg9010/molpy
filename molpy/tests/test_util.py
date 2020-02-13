0dfc42990d06cf2016efdef1e15720c06d63a465
alexg9010 <alex.gos90@gmail.com>
2020-02-13 09:39:02 +0100

    add test for reading xyz

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -1,6 +1,7 @@
 
 import pytest
 import molpy
+import numpy as np
 
 
 @pytest.mark.parametrize("point1, point2, bench", [
@@ -16,3 +17,18 @@ def test_distance(point1, point2, bench):
 
 def test_distance_failure():
     assert molpy.util.distance([0], [3]) != 5
+
+
+@pytest.mark.parametrize(
+    "molecule, com",
+    [
+        ("water", [9.81833333, 7.60366667, 12.673]),
+        ("benzene", [-1.4045, 0, 0])
+    ],
+)
+def test_read_xyz(molecule, com):
+
+    mol = molpy.data.get_molecule(molecule)
+    print(np.mean(mol["geometry"], axis=0))
+    print(com)
+    assert np.allclose(np.mean(mol["geometry"], axis=0), com)
